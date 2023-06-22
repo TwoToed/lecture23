6409a8568d331efa13f853e4cd54eb5f3afc2b4e
TwoToed <PeranutYl@gmail.com>
2023-06-22 14:15:31 +0800

    first commit

---
+++ src/main/java/paf/lecture23/Lecture23Application.java
@@ -0,0 +1,13 @@
+package paf.lecture23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture23Application.class, args);
+	}
+
+}
